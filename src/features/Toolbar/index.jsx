971eb7ef01f37f45a17889abfd18adaf0fdf4ca3
mohamedyasser27 <my2722000@gmail.com>
2023-02-27 03:57:43 +0200

    Move the background-color,color transition to a * selector
    i did that to avoid redundancy

--- src/features/Toolbar/index.jsx
+++ src/features/Toolbar/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Toolbar.scss";
 import SearchBar from "./components/SearchBar/SearchBar";
+import "./Toolbar.scss";
+import DropDown from "./components/DropDown/DropDown";
 export default function Toolbar() {
   return (
     <div className="toolbar">
-     <SearchBar/>
+      <SearchBar />
+      <DropDown />
     </div>
   );
 }
